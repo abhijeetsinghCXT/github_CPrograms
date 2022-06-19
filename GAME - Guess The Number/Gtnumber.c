854e5b78d0e1b27bd60cb7477944d57fc45c02cf
Abhijeet Singh <106686793+abhijeetsinghCXT@users.noreply.github.com>
2022-06-19 14:42:15 +0530

    New Game added

---
+++ GAME - Guess The Number/Gtnumber.c
@@ -0,0 +1,51 @@
+/* 
+
+This Game is developed by Abhijeet Singh in C Language under Basic C Programs 
+
+*/
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, guess, attempts = 0;
+    srand((time(0)));
+
+    printf("Welcome to Guess the Number Game\n");
+
+    num = rand() % 100 + 1;
+    do
+    {
+        if (attempts>0)
+        {
+            goto label;
+        }
+        
+        printf("Enter your Guess between 1 to 100\n");
+     label: 
+        scanf("%d", &guess);
+        if (guess < num)
+        {
+            printf("Enter Higher Number\n");
+        }
+        else if (guess > num)
+        {
+            printf("Enter Lower Number\n");
+        }
+        else if (guess==num)
+        {
+           printf("Your guess %d is correct\n", num);
+        }
+        
+            
+
+        
+        attempts++;
+
+    } while (guess != num);
+    printf("You take %d attempts for answer!\n", attempts);
+
+    return 0;
+}
--- Triangular Star Patterns/star.c
+++ Triangular Star Patterns/star.c
@@ -1,23 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void star(int n)
 {
- 
-int i;
-    for (i = 0; i <n; i++)
-    { for (int j = 0; j <= i; j++)
+
+    int i;
+    for (i = 0; i < n; i++)
     {
-       printf("*");
-    }
-    
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+
         printf("\n");
     }
-
-
-
 }
 
- int main(){  
+int main()
+{
 
     int n;
     printf("How many rows do you want?\n");
